ac77f545947d6eb28aca838d35bb8cad76fc5983
lonny <lonny.p@eyebuydirect.com>
2017-04-26 18:25:33 +0800

    change

--- location.php
+++ location.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ "浙江省", "安徽省", "福建省", "江西省", "山东省", "河南省", "湖北省", "湖南省", "广东省", "广西壮族自治区", "海南省", "重庆市", "四川省", "贵州省", "云南省", "西藏自治区", "陕西省", "甘肃省", "青海省", "宁夏回族自治区", "新疆维吾尔自治区",
+*/
+$alreadyData = array(
+    "辽宁省", "吉林省", "黑龙江省", "上海市", "江苏省", "浙江省", "安徽省", "福建省", "江西省", "山东省", "河南省", "湖北省", "湖南省", "广东省", "广西壮族自治区", "海南省", "重庆市", "四川省", "贵州省", "云南省", "西藏自治区", "陕西省", "甘肃省", "青海省", "宁夏回族自治区", "新疆维吾尔自治区", 
+);
 $pdo = linkPdo(array('dbName' => 'db_main', 'root' => 'root', 'password' => 'root'));
 
 $sql = "SELECT toponymy_id, toponymy_parent_id FROM t_toponymy WHERE toponymy_order = ''";
@@ -43,6 +49,9 @@ foreach ($link as $key => $value) {
 $orderArr = array(18, 11);
 $data_0 = curlMulti($data_0);
 foreach ($data_0 as $key => $value) {
+    if (!in_array($key, $alreadyData)) {
+        continue;
+    }
     $parentId0 = 18;
     $sql = "SELECT toponymy_id FROM t_toponymy WHERE toponymy_name = '%s' AND toponymy_status = 1 AND toponymy_order = '%s'";
     $result = $pdo->query(sprintf($sql, $key, implode(",", $orderArr)));
